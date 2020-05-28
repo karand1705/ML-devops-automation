d37e904e7d1138cd91335d1ed56bcb3d9ad7db67
karand1705 <58079053+karand1705@users.noreply.github.com>
2020-05-29 02:40:08 +0530

    third update

--- mainmodel.py
+++ mainmodel.py
@@ -11,6 +11,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
+import os
 
 
 # In[2]:
@@ -102,3 +103,4 @@ accuracy=score[1]*100
 file = open("accuracy.txt","w+")
 file.write(str(accuracy))
 file.close()
+os.system("mv /accuracy.txt /mycode")
